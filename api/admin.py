4c9b809c78c454d6cc5c0363a31fdb442da0992a
nayankothari <64473383+nayankothari@users.noreply.github.com>
2022-05-26 23:23:06 +0530

    changes in admin for list filters.

--- api/admin.py
+++ api/admin.py
@@ -5,15 +5,22 @@ from api.models import *
 class KeyMgmtSearch(admin.ModelAdmin):
     search_fields = ["public_key", "head_office", "number", "email", "system_id"]
     readonly_fields = ["created"]
-
+    list_filter = ["public_key", "number", "system_id"]
+        
 class RewardPoint(admin.ModelAdmin):
     readonly_fields = ["created", "updated_date"]
+    list_filter = ["office", "branch"]
+    search_fields = ["office", "branch"]
 
 class DiscountPoint(admin.ModelAdmin):
     readonly_fields = ["created", "updated_date"]
-
+    list_filter = ["office", "branch"]
+    search_fields = ["office", "branch"]
+    
 class CustomerDetail(admin.ModelAdmin):
     readonly_fields = ["created", "updated_date"]
+    list_filter = ["office", "branch", "number"]
+    search_fields = ["office", "branch", "number"]
 
 admin.site.register(RewardPointSystem, RewardPoint)
 admin.site.register(DiscountPointSystem, DiscountPoint)
